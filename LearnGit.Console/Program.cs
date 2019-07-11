1132e34cc8865c13c76db73d51fd6fc024c72c9b
qzl <1310373524@qq.com>
2019-07-11 09:34:28 +0800

    1

---
+++ LearnGit.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnGit.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 2;
+        }
+    }
+}
